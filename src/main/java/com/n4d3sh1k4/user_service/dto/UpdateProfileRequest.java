8607af1d7b271f74aa77efa926e028ba3cb78f93
N4d3sh <vusezet@gmail.com>
2026-06-27 23:36:21 +0300

    fix: fix phone validation

--- src/main/java/com/n4d3sh1k4/user_service/dto/UpdateProfileRequest.java
+++ src/main/java/com/n4d3sh1k4/user_service/dto/UpdateProfileRequest.java
@@ -14,7 +14,7 @@ public record UpdateProfileRequest(
              message = "The surname must be in Cyrillic and may contain a hyphen.")
     String lastName,
 
-    @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Invalid phone format.")
+    @Pattern(regexp = "^79[0-9]{9}$", message = "The phone number must be 11 digits long and start with 79")
     String phone,
 
     @Size(min = 2, max = 50)
